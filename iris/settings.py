e43182e69e1e4d4535436ae11af38e358a8d72b9
ShrishMaruge <shrish.maruge99@gmail.com>
2025-06-13 13:13:58 +0530

    Update settings.py

--- iris/settings.py
+++ iris/settings.py
@@ -116,9 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # required by Render
+
+# Optional for better handling:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
